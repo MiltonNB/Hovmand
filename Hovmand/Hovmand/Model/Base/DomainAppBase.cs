368f2ff2b13f6b2f9d9289956a34d6a8188a1f47
MaltePovlsen <32706692+MaltePovlsen@users.noreply.github.com>
2018-05-02 11:22:11 +0200

    Domain Base class

---
+++ Hovmand/Hovmand/Model/Base/DomainAppBase.cs
@@ -0,0 +1,13 @@
+﻿using Data.Transformed.Implementation;
+
+namespace Hovmand.Model.Base
+{
+    public abstract class DomainAppBase : CopyableWithDefaultValuesBase
+    {
+        public int Id
+        {
+            get { return Key; }
+            set { Key = value; }
+        }
+    }
+}
