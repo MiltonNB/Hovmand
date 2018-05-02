3218bfecf879bf5648ed758c77a7c6cdfec6ac5b
zirr <31447312+zirriously@users.noreply.github.com>
2018-05-02 12:08:30 +0200

    Changed model classes to follow the basic convention instead of autoproperties

--- Hovmand/Hovmand/Model/App/Pipeline.cs
+++ Hovmand/Hovmand/Model/App/Pipeline.cs
@@ -4,20 +4,49 @@ namespace Hovmand.Model.App
 {
     public class Pipeline
     {
+        private int _pipelineID;
+        private StatusEnum _status;
+        private string _information;
+        private string _date;
+        private string _lastEditDate;
+
         public Pipeline(int pipelineID, StatusEnum status, string information, string date, string lastEditDate)
         {
-            PipelineID = pipelineID;
-            Status = status;
-            Information = information;
-            Date = date;
-            LastEditDate = lastEditDate;
+            _pipelineID = pipelineID;
+            _status = status;
+            _information = information;
+            _date = date;
+            _lastEditDate = lastEditDate;
         }
 
-        public int PipelineID { get; set; }
-        public StatusEnum Status { get; set; }
-        public string Information { get; set; }
-        public string Date { get; set; }
-        public string LastEditDate { get; set; }
+        public int PipelineID
+        {
+            get { return _pipelineID; }
+            set { _pipelineID = value; }
+        }
 
+        public StatusEnum Status
+        {
+            get { return _status; }
+            set { _status = value; }
+        }
+
+        public string Information
+        {
+            get { return _information; }
+            set { _information = value; }
+        }
+
+        public string Date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+
+        public string LastEditDate
+        {
+            get { return _lastEditDate; }
+            set { _lastEditDate = value; }
+        }
     }
 }
--- Hovmand/Hovmand/Model/App/User.cs
+++ Hovmand/Hovmand/Model/App/User.cs
@@ -2,17 +2,41 @@
 {
     public class User
     {
-        public User(string userID, string firstName, string lastName, string title)
+        private int _userID;
+        private string _firstName;
+        private string _lastName;
+        private string _title;
+
+        public User(int userID, string firstName, string lastName, string title)
         {
-            UserID = userID;
-            FirstName = firstName;
-            LastName = lastName;
-            Title = title;
+            _userID = userID;
+            _firstName = firstName;
+            _lastName = lastName;
+            _title = title;
         }
 
-        public string UserID { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Title { get; set; }
+        public int UserID
+        {
+            get { return _userID; }
+            set { _userID = value; }
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
     }
 }
