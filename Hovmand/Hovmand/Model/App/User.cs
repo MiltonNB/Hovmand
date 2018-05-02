d89d17a8ee53352c236829eeb06d5a1a07a4cec1
zirr <31447312+zirriously@users.noreply.github.com>
2018-05-02 11:03:11 +0200

    User

--- Hovmand/Hovmand/Model/App/User.cs
+++ Hovmand/Hovmand/Model/App/User.cs
@@ -2,17 +2,17 @@
 {
     public class User
     {
-        private string _userID;
-        private string _firstName;
-        private string _lastName;
-        private string _title;
-
         public User(string userID, string firstName, string lastName, string title)
         {
-            _userID = userID;
-            _firstName = firstName;
-            _lastName = lastName;
-            _title = title;
+            UserID = userID;
+            FirstName = firstName;
+            LastName = lastName;
+            Title = title;
         }
+
+        public string UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
     }
 }
