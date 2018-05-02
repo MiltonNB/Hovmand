f8610332d9c0c0fe4b0fa39e985f04a0e338294c
zirr <31447312+zirriously@users.noreply.github.com>
2018-05-02 10:30:34 +0200

    Added barebones user class (model)

---
+++ Hovmand/Hovmand/Model/App/User.cs
@@ -0,0 +1,18 @@
+﻿namespace Hovmand.Model.App
+{
+    public class User
+    {
+        private string _userID;
+        private string _firstName;
+        private string _lastName;
+        private string _title;
+
+        public User(string userID, string firstName, string lastName, string title)
+        {
+            _userID = userID;
+            _firstName = firstName;
+            _lastName = lastName;
+            _title = title;
+        }
+    }
+}
