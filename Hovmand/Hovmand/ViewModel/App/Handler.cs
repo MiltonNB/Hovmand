a7b180e25ccf11ffe9e9cc26a37257f0716f91e9
zirr <31447312+zirriously@users.noreply.github.com>
2018-05-02 10:25:28 +0200

    Handler class, temporary methods + method types. Pretty much everything is just placeholder!

---
+++ Hovmand/Hovmand/ViewModel/App/Handler.cs
@@ -0,0 +1,52 @@
+﻿namespace Hovmand.ViewModel.App
+{
+    public class Handler
+    {
+        // TODO - method types are temporary
+
+        public void GetLeads()
+        {
+            
+        }
+
+        public void SelectLeads()
+        {
+            
+        }
+
+        public void SaveOffer(int selectedLead)
+        {
+
+        }
+
+        public string GetLeadByID(int ID)
+        {
+            return "";
+        }
+
+        public void EditLock(int ID)
+        {
+            
+        }
+
+        public void EditUnlock(int ID)
+        {
+            
+        }
+
+        private void SendEmail(string recipient)
+        {
+            
+        }
+
+        public void ChangeLeadStatus(int LeadID)
+        {
+            
+        }
+
+        public string SaveConfirmation()
+        {
+            return "";
+        }
+    }
+}
