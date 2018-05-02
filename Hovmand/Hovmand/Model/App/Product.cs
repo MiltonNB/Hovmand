d4fafc34b26d133626eb3d6e28006974e03388e2
zirr <31447312+zirriously@users.noreply.github.com>
2018-05-02 11:08:35 +0200

    Product

---
+++ Hovmand/Hovmand/Model/App/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace Hovmand.Model.App
+{
+    public class Product
+    {
+        public Product(int productID, string title, string information, float price)
+        {
+            ProductID = productID;
+            Title = title;
+            Information = information;
+            Price = price;
+        }
+
+        public int ProductID { get; set; }
+        public string Title { get; set; }
+        public string Information { get; set; }
+        public float Price { get; set; }
+    }
+}
