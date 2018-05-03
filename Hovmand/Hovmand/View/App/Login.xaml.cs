15db7e98908092a84cfd028fa01144ee55a67236
MiltonNB <31879870+MiltonNB@users.noreply.github.com>
2018-05-03 11:49:44 +0200

    Login Page

--- Hovmand/Hovmand/View/App/Login.xaml.cs
+++ Hovmand/Hovmand/View/App/Login.xaml.cs
@@ -12,26 +12,29 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using Hovmand.View.App;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace Hovmand
+namespace Hovmand.View.App
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Login : Page
     {
-        public MainPage()
+        public Login()
         {
             this.InitializeComponent();
-            Loaded += MainPage_Loaded;
         }
 
-        private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        private void SignInButton_Click(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(Login));
+            ErrorMessage.Text = "";
+        }
+
+        private void ForgotPasswordButton_Click(object sender, PointerRoutedEventArgs e)
+        {
+            ErrorMessage.Text = "";
         }
     }
 }
