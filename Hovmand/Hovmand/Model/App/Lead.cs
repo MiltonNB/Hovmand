8a78f255fd626e4bc77263c23ccd9b660ff0429a
Mikx0125 <31879838+Mikx0125@users.noreply.github.com>
2018-05-03 09:21:42 +0200

    Lead klasse

---
+++ Hovmand/Hovmand/Model/App/Lead.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hovmand.Model.App
+{
+    public class Lead
+    {
+
+        public Lead(int leadID, StatusEnum status, string information)
+        {
+            LeadID = leadID;
+            Status = status;
+            Information = information;
+
+        }
+
+        public int LeadID { get; set; }
+        public StatusEnum Status { get; set; }
+        public string Information { get; set; }
+    }
+}
