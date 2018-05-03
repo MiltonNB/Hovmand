dc26d329a11efd758ea6850cc117c681978ccb5e
zirr <chri650t@edu.easj.dk>
2018-05-03 13:31:34 +0200

    Kommentarer

--- Hovmand/Hovmand/Model/App/PasswordHash.cs
+++ Hovmand/Hovmand/Model/App/PasswordHash.cs
@@ -5,23 +5,23 @@ namespace Hovmand.Model.App
 {
     public class PasswordHash
     {
-        private SHA1CryptoServiceProvider _sha1Provider = new SHA1CryptoServiceProvider();
+        private SHA1CryptoServiceProvider _sha1Provider = new SHA1CryptoServiceProvider(); //Ny service til at hashe
 
-        public string HashString(string input)
+        public string HashString(string input) //Returnerer hash fra string
         {
-            var inputBytes = Encoding.ASCII.GetBytes(input);
-            var inputHashed = _sha1Provider.ComputeHash(inputBytes);
-            var outputHashed = Encoding.ASCII.GetString(inputHashed);
-            return outputHashed;
+            var inputBytes = Encoding.ASCII.GetBytes(input); //Starter med at lave input string om til bytes, da du ikke kan hashe strings, men derimod bytes
+            var inputHashed = _sha1Provider.ComputeHash(inputBytes); //Laver det egentlige hash fra bytes
+            var outputHashed = Encoding.ASCII.GetString(inputHashed); //Laver det tilbage om til en string så det er læseligt
+            return outputHashed; //Returnerer den endelige string
         }
 
-        public bool MatchString(string input, string toMatch)
+        public bool MatchString(string input, string toMatch) //Sammenligner 2 strings, hvor den hasher input
         {
-            var inputBytes = Encoding.ASCII.GetBytes(input);
-            var inputHashed = _sha1Provider.ComputeHash(inputBytes);
-            var inputHashedString = Encoding.ASCII.GetString(inputHashed);
+            var inputBytes = Encoding.ASCII.GetBytes(input); //Laver input string om til bytes igen
+            var inputHashed = _sha1Provider.ComputeHash(inputBytes); //Hasher bytes som er taget fra input string
+            var inputHashedString = Encoding.ASCII.GetString(inputHashed); //Tilbage til en string
 
-            if (inputHashedString == toMatch)
+            if (inputHashedString == toMatch) //Hvis de 2 matcher
             {
                 return true;
             }
