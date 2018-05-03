b88f72894be907d2180010e8a9c1a8e5f70c1f0f
zirr <chri650t@edu.easj.dk>
2018-05-03 13:25:48 +0200

    Passwordhash klasse

---
+++ Hovmand/Hovmand/Model/App/PasswordHash.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Hovmand.Model.App
+{
+    public class PasswordHash
+    {
+        private SHA1CryptoServiceProvider _sha1Provider = new SHA1CryptoServiceProvider();
+
+        public string HashString(string input)
+        {
+            var inputBytes = Encoding.ASCII.GetBytes(input);
+            var inputHashed = _sha1Provider.ComputeHash(inputBytes);
+            var outputHashed = Encoding.ASCII.GetString(inputHashed);
+            return outputHashed;
+        }
+
+        public bool MatchString(string input, string toMatch)
+        {
+            var inputBytes = Encoding.ASCII.GetBytes(input);
+            var inputHashed = _sha1Provider.ComputeHash(inputBytes);
+            var inputHashedString = Encoding.ASCII.GetString(inputHashed);
+
+            if (inputHashedString == toMatch)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
