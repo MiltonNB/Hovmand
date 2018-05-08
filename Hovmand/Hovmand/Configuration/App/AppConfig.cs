019c6890dbfb769ebbcdca047bd3370944e7d197
MiltonNB <31879870+MiltonNB@users.noreply.github.com>
2018-05-08 11:02:44 +0200

    .MainPage Setup + AppConfig file & App Resources

---
+++ Hovmand/Hovmand/Configuration/App/AppConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+using AddOns.Images.Implementation;
+using AddOns.Images.Interfaces;
+using Extensions.AddOns.Implementation;
+using Hovmand.ViewModel.App;
+using Hovmand.View.App;
+
+namespace Hovmand.Configuration.App
+{
+    //public static void Setup(Page mainPage, Frame appFrame)
+    //{
+    //    appFrame.Navigate(typeof(FileView));
+    //    ((AppViewModel)mainPage.DataContext).SetAppFrame(appFrame);
+    //}
+}
--- Hovmand/Hovmand/View/App/MainPage.xaml.cs
+++ Hovmand/Hovmand/View/App/MainPage.xaml.cs
@@ -1,22 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using Hovmand.View.App;
+﻿using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace Hovmand
+namespace Hovmand.View.App
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
@@ -26,12 +12,18 @@ namespace Hovmand
         public MainPage()
         {
             this.InitializeComponent();
-            Loaded += MainPage_Loaded;
+            //AppConfig.Setup(this, AppFrame);
         }
 
-        private void MainPage_Loaded(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(LoginPage));
-        }
+        //public MainPage()
+        //{
+        //    this.InitializeComponent();
+        //    Loaded += MainPage_Loaded;
+        //}
+
+        //private void MainPage_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    Frame.Navigate(typeof(LoginPage));
+        //}
     }
 }
