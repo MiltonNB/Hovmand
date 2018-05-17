80c90d442ec1694c4cd5b48b24ba7378287f492d
MaltePovlsen <32706692+MaltePovlsen@users.noreply.github.com>
2018-05-17 11:26:55 +0200

    DataViewModel

--- Hovmand/Hovmand/ViewModel/Data/ContactDataViewModel.cs
+++ Hovmand/Hovmand/ViewModel/Data/ContactDataViewModel.cs
@@ -1,7 +1,64 @@
-﻿namespace Hovmand.ViewModel.Data
+﻿using Windows.ApplicationModel.Store.Preview.InstallControl;
+using Hovmand.Model.Domain;
+using ViewModel.Data.Implementation;
+
+namespace Hovmand.ViewModel.Data
 {
-    public class ContactDataViewModel
+    public class ContactDataViewModel : DataViewModelBase<Contact>
     {
-        
+        public ContactDataViewModel(Contact obj) : base (obj)
+        {
+            
+        }
+
+        public int ContactId
+        {
+            get { return DataObject.ContactId; }
+            set
+            {
+                DataObject.ContactId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string FirstName
+        {
+            get { return DataObject.Firstname; }
+            set
+            {
+                DataObject.Firstname = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LastName
+        {
+            get { return DataObject.Lastname; }
+            set
+            {
+                DataObject.Lastname = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Mail
+        {
+            get { return DataObject.Mail; }
+            set
+            {
+                DataObject.Mail = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Phone
+        {
+            get { return DataObject.Phone; }
+            set
+            {
+                DataObject.Phone = value;
+                OnPropertyChanged();
+            }
+        }
     }
 }
