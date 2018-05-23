910fb1bd49599e7ea2b35f48b007008b54a8329b
MaltePovlsen <32706692+MaltePovlsen@users.noreply.github.com>
2018-05-23 10:36:42 +0200

    DataViewModel

--- Hovmand/Hovmand/ViewModel/Data/CustomerDataViewModel.cs
+++ Hovmand/Hovmand/ViewModel/Data/CustomerDataViewModel.cs
@@ -1,7 +1,22 @@
-﻿namespace Hovmand.ViewModel.Data
+﻿using Hovmand.Model.Domain;
+using Hovmand.ViewModel.Base;
+
+namespace Hovmand.ViewModel.Data
 {
-    public class CustomerDataViewModel
+    public class CustomerDataViewModel : DataViewModelBase
     {
-        
+        private Customer _domainObject;
+
+        public CustomerDataViewModel(Customer domainObject)
+        {
+            _domainObject = domainObject;
+        }
+
+        public Customer CustomerDomainObject
+        {
+            get { return _domainObject; }
+        }
+
+        public CustomerId
     }
 }
