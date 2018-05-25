91ad3708c7f78447c051141cc29c70d2282a19b8
Nicholas Milton Bauer <nich2844@edu.easj.dk>
2018-05-25 12:02:47 +0200

    .

--- Hovmand/Hovmand/View/Domain/LeadsPage.xaml.cs
+++ Hovmand/Hovmand/View/Domain/LeadsPage.xaml.cs
@@ -20,9 +20,9 @@ namespace Hovmand.View.Domain
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class ActiveLeads : Page
+    public sealed partial class LeadsPage : Page
     {
-        public ActiveLeads()
+        public LeadsPage()
         {
             this.InitializeComponent();
         }
