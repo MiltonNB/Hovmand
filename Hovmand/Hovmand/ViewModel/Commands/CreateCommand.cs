9adcc804eb29ad39d21d2f622cfa2ab6a4bc2e51
MaltePovlsen <32706692+MaltePovlsen@users.noreply.github.com>
2018-05-25 09:38:41 +0200

    create command

---
+++ Hovmand/Hovmand/ViewModel/Commands/CreateCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Input;
+using Hovmand.Model.Catalog.Base;
+
+namespace Hovmand.ViewModel.Commands
+{
+    public class CreateCommand : ICommand
+    {
+        private CatalogBase<Contact> _contactCatalog;
+
+        public CreateCommand(CatalogBase<Contact> contactCatalog)
+        {
+            _contactCatalog = contactCatalog;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _contactCatalog.Create(new Contact()
+            {
+                Firstname = "",
+
+            });
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
