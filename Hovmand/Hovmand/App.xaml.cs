ea35ace1cd8f20c35687872e1dfcf01febb340a5
Nicholas Milton Bauer <nich2844@edu.easj.dk>
2018-05-29 16:07:01 +0200

    leadpage

--- Hovmand/Hovmand/App.xaml.cs
+++ Hovmand/Hovmand/App.xaml.cs
@@ -56,7 +56,7 @@ namespace Hovmand
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(View.App.MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(View.App.LeadPage), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
